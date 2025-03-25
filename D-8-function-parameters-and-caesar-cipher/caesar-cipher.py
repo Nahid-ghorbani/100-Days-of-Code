c9d7c35a428a5af4b534217c778df7785a5639dd
Nahid <ghorbani.nca@gmail.com>
2025-03-25 23:54:52 +0330

    adding while loop and check non alphabet chars

--- D-8-function-parameters-and-caesar-cipher/caesar-cipher.py
+++ D-8-function-parameters-and-caesar-cipher/caesar-cipher.py
@@ -1,20 +1,28 @@
+from art import logo
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-alphabet_len = len(alphabet)
+print(logo)
+run_code = "yes"
 
 def caesar(original_text, shift_amount, encode_or_decode):
+    alphabet_len = len(alphabet)
     if encode_or_decode == "decode":
         shift_amount *= -1
 
     output_text = ""
     for letter in original_text:
-        shifted_position = alphabet.index(letter) + shift_amount
-        shifted_position %= alphabet_len #we use % so basically there is no limit for shift number
-        output_text += alphabet[shifted_position]
+        if letter in alphabet:
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= alphabet_len #we use % so basically there is no limit for shift number
+            output_text += alphabet[shifted_position]
+        else:
+            output_text += letter
     print(f"Here is the {encode_or_decode}d result: {output_text}")
 
-
-caesar(text, shift, direction)
+while run_code == "yes":
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(text, shift, direction)
+    run_code = input("Type yes if you want to go again. otherwise type no: \n").lower()
