f46c043b65b51db236ab9e34e100b6f2a0e88117
Nahid <ghorbani.nca@gmail.com>
2025-03-27 01:24:57 +0330

    developed code to a organized version

--- D-9-dictionaries-nesting-and-secret-auction/secret-auction.py
+++ D-9-dictionaries-nesting-and-secret-auction/secret-auction.py
@@ -1,25 +1,21 @@
 from art import logo
 
 bids_dictionary = {}
-
 calculation_completed = False
+
+def find_highest_bidder(bidding_dictionary):
+    max_bidder = max(bidding_dictionary)
+    print(f"The winner is {max_bidder} with a bid of ${bidding_dictionary[max_bidder]}")
+
+print(logo)
+
 while not calculation_completed:
     user_name = input("what is your name?\n")
     bid = int(input("what is your bid?\n$"))
     bids_dictionary[user_name] = bid
-    print(bids_dictionary)
     add_new_bid = input("Are there any other bidders? Type 'yes' or 'no': ").lower()
     if add_new_bid == "no":
         calculation_completed = True
+        find_highest_bidder(bids_dictionary)
     else:
         print("\n" * 20)
-
-
-max_bid = 0
-winner = ""
-for key in bids_dictionary:
-    if bids_dictionary[key] > max_bid:
-        max_bid = bids_dictionary[key]
-        winner = key
-
-print(f"The winner is {winner} with a bid of ${max_bid}")
