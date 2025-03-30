c8d2510fb6c75ec3dad7f0aebf1c0fcef8bedb08
Nahid <ghorbani.nca@gmail.com>
2025-03-31 02:09:01 +0330

    change code based on course

--- D-12-Scopes-and-number-guessing-game/number-guessing-game.py
+++ D-12-Scopes-and-number-guessing-game/number-guessing-game.py
@@ -1,42 +1,44 @@
 from art import logo
 import random
 
+EASY_ATTEMPTS_TURN = 10
+HARD_ATTEMPTS_TURN = 5
+
+def compare_guess_to_answer(user_guess, random_answer, turns):
+    if user_guess == random_answer:
+        print(f"You got it! The answer was {random_answer}")
+    elif random_answer > user_guess:
+        print("Too high.")
+        return turns - 1
+    else:
+        print("Too low")
+        return turns - 1
+
+def set_attempts():
+    level = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+    if level == "easy":
+        return EASY_ATTEMPTS_TURN
+    else:
+        return HARD_ATTEMPTS_TURN
+
 def play_game():
     print(logo)
     print("Welcome to the Number Guessing Game!")
     print("I'm thinking of a number between 1 and 100.")
-    lives = 0
+    turns = set_attempts()
 
-    game_difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
-    if game_difficulty == "easy":
-        lives = 10
-    else:
-        lives = 5
-
-
-    game_over = False
     random_number = random.randint(1, 100)
+    guess_number = 0
 
-    while not game_over:
-        print(f"You have {lives} attempts remaining to guess the number.")
+    while guess_number != random_number:
+        print(f"You have {turns} attempts remaining to guess the number.")
         guess_number = int(input("Make a guess: "))
-        lives -= 1
+        turns = compare_guess_to_answer(guess_number , random_number , turns)
 
-        if guess_number == random_number:
-            print(f"You got it! The answer was {random_number}")
-            game_over = True
-
-        elif lives == 0:
+        if turns == 0:
             print("You've run out of guesses. Refresh the page to run again.")
-            game_over = True
-
-        elif guess_number > random_number:
-            print("Too high.")
-            print("Guess again.")
-
+            return
         else:
-            print("Too low")
             print("Guess again.")
-
 
 play_game()
