8bca0ac1bb93e5c8d4c6089682c599c91c558267
Nahid <ghorbani.nca@gmail.com>
2025-04-02 22:48:39 +0330

    the quiz project - cinema quiz

---
+++ D-17-the-quiz-project-and-benefits-of-oop/data.py
@@ -0,0 +1,78 @@
+question_data = [
+    {
+        "type":"boolean",
+        "difficulty":"easy",
+        "category":"Entertainment: Film",
+        "question":"In Alfred Hitchcock's film 'Psycho' it is said he used chocolate syrup to simulate the blood in the famous shower scene.",
+        "correct_answer":"True","incorrect_answers":["False"]
+    },
+    {   "type":"boolean",
+        "difficulty":"easy",
+        "category":"Entertainment: Film",
+        "question":"The 2010 film 'The Social Network'; is a biographical drama film about MySpace founder Tom Anderson.",
+        "correct_answer":"False",
+        "incorrect_answers":["True"]
+    },
+    {   
+        "type":"boolean",
+        "difficulty":"easy",
+        "category":"Entertainment: Film",
+        "question":"In the original Star Wars trilogy, Alec Guinness provided the voice for Darth Vader.",
+        "correct_answer":"False",
+        "incorrect_answers":["True"]
+    },
+    {
+        "type":"boolean",
+        "difficulty":"easy",
+        "category":"Entertainment: Film",
+        "question":"The movie 'The Nightmare before Christmas' was all done with physical objects.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+    },
+    {
+        "type":"boolean",
+        "difficulty":"easy",
+        "category":"Entertainment: Film",
+        "question":"'Foodfight!' earned less than $80,000 at box office.",
+        "correct_answer":"True","incorrect_answers":["False"]
+    },
+    {
+        "type":"boolean",
+        "difficulty":"easy",
+        "category":"Entertainment: Film",
+        "question":"The film '2001: A Space Odyssey' was released on December 31st, 2000.",
+        "correct_answer":"False",
+        "incorrect_answers":["True"]
+    },
+    {
+        "type":"boolean",
+        "difficulty":"easy",
+        "category":"Entertainment: Film",
+        "question":"Ewan McGregor did not know the name of the second prequel film of Star Wars during and after filming.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+    },
+    {
+        "type":"boolean",
+        "difficulty":"easy",
+        "category":"Entertainment: Film",
+        "question":"Han Solo's co-pilot and best friend, 'Chewbacca', is an Ewok.",
+        "correct_answer":"False",
+        "incorrect_answers":["True"]
+    },
+    {
+        "type":"boolean",
+        "difficulty":"easy",
+        "category":"Entertainment: Film",
+        "question":"The word 'Inception' came from the 2010 blockbuster hit 'Inception'.",
+        "correct_answer":"False",
+        "incorrect_answers":["True"]
+    },
+    {
+        "type":"boolean",
+        "difficulty":"easy",
+        "category":"Entertainment: Film",
+        "question":"Actor Tommy Chong served prison time.",
+        "correct_answer":"True","incorrect_answers":["False"]
+    }
+]
